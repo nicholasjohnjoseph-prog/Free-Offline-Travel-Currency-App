3a8fa9349177ff6376d7a504604c7a4c579ea770
nicholasjohnjoseph-prog <nicholasjohnjoseph@gmail.com>
2026-02-05 17:39:33 -0600

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-// config.js
-const API_KEY =
-    '96e49f1454195e418c84e5f6';
-// Or if using an object: 
-// window. CONFIG = { API_KEY:
-'96e49f1454195e418c84e5f6' };
+const API_KEY = '96e49f1454195e418c84e5f6';
